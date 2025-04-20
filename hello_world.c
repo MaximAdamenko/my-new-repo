71430daf84f3c7ee5e03a504fd79d13d4ef84d76
Maxim Adamenko <maxmmm10@gmail.com>
2025-04-20 15:40:51 +0300

    new
    added new code in c

--- hello_world.c
+++ hello_world.c
@@ -1,4 +1,22 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int checkNumOfDigits(int num);
+
+
 int main(){
     printf("Hello, World!\n");
+    printf("enter a number: ");
+    int num;
+    scanf("%d\n", &num);
+    printf("The number you entered is: %d\n", num);
+    printf("The num of digits in the number is: %d\n", checkNumOfDigits(num));
     return 0;
+}
+
+int checkNumOfDigits(int num){
+    return (int)(log10(num) + 1);
 }
